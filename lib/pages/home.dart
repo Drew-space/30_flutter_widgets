134da3941327fd64a25daa044a5e4de7d3cf1f61
Drew <chukwukaenudeme@gmail.com>
2026-05-21 19:39:22 +0100

    Refactor HomePage layout to use Column and simplify Container structure

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,19 +6,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          alignment: Alignment(0.0, 0.0),
-          height: 200,
-          width: 200,
-
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-
-            border: Border.all(color: Colors.deepPurpleAccent, width: 2),
-          ),
-          child: Text("Hello"),
-        ),
+      body: Column(
+        children: [Container(height: 200, color: Colors.deepPurple)],
       ),
     );
   }
