0c9d840c6da723bdaa8c608d72ae93c883f53219
Drew <chukwukaenudeme@gmail.com>
2026-05-21 20:43:32 +0100

    Refactor HomePage layout to replace Row with ListView for better scrolling and maintain consistent padding for child containers

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,16 +6,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-
+      body: ListView(
         children: [
-          Expanded(child: Container(width: 100, color: Colors.deepPurple[600])),
-
-          Container(height: 100, width: 100, color: Colors.deepPurple[400]),
-
-          Container(height: 100, width: 100, color: Colors.deepPurple[200]),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(height: 200, color: Colors.deepPurple[200]),
+          ),
         ],
       ),
     );
