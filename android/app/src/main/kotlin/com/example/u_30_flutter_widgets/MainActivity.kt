cfd3f1a5c5ef4081e1c9329848f27c6e04f08266
Drew <chukwukaenudeme@gmail.com>
2026-05-21 16:24:40 +0100

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and support for Windows 10 and 11.
    - Implemented console creation and output redirection in `utils.cpp` to allow debugging output in the console.
    - Added utility functions for command line argument handling and UTF-16 to UTF-8 string conversion.
    - Created `win32_window` class to manage a high DPI-aware Win32 window, including theme updates based on system settings.
    - Registered window class and handled messages for window creation, destruction, and resizing.
    - Ensured compatibility with dark mode by checking system theme preferences.

---
+++ android/app/src/main/kotlin/com/example/u_30_flutter_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_30_flutter_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(height: 200, width: 200, color: Colors.deepPurple),
+      ),
+    );
+  }
+}
